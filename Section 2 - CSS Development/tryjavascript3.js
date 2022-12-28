0600f0e36f5df69a1a59b7080a3e0f90d2e79682
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2022-12-28 17:11:47 +0000

    javascript07

---
+++ Section 2 - CSS Development/tryjavascript3.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// ================================================================================
+//
+function bodyload7() {
+    var x=document.getElementsByName("pay");
+    alert("Total number of payment methods is: " + x.length);
+}
