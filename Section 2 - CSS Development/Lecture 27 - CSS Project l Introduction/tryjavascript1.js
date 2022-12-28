a2f8b7627db75c44053d62eb27e2a3bc8301edbf
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2022-12-28 16:59:31 +0000

    Building on On javascript

---
+++ Section 2 - CSS Development/Lecture 27 - CSS Project l Introduction/tryjavascript1.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// ================================================================================
+//
+function bodyload5() {
+    var x=document.getElementsByTagName("img");
+    alert("total no. of images in page: " + x.length);
+}
---
+++ Section 2 - CSS Development/Lecture 27 - CSS Project l Introduction/tryjavascript2.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// ================================================================================
+//
+function bodyload6() {
+    var x=document.getElementsByClassName("bstyle");
+    alert("total no. of elements using bstyle: " + x.length);
+}
