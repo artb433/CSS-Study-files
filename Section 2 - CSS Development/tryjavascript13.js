f758dab1372185ef2bde67662f49b44449252f7f
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-07 18:56:54 +0000

    Javascript practice

---
+++ Section 2 - CSS Development/tryjavascript13.js
@@ -0,0 +1,8 @@
+// Global scope
+// c:\Users\tahiru\Downloads\Compressed\CSS-Required-Files\Section 2 - CSS Development\tryjavascript13.js (javascript) 
+//
+
+var r = 90;
+function f3(){
+    document.write("r= "+r);
+}
