241eda87e143fc0a20211b0992cebc95e971537c
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-09 02:38:41 +0000

    Javascript practice

---
+++ Section 2 - CSS Development/tryjavascript15.js
@@ -0,0 +1,9 @@
+function tryUndefined(){
+    var x;
+    if (x==undefined) {
+        document.write("Datatype is undefined, not known")
+    } else {
+        document.write("x= "+x);
+    }
+}
+tryUndefined();
---
+++ Section 2 - CSS Development/tryjavascript16.js
@@ -0,0 +1,5 @@
+function javascriptArrayTry(){
+    var product= [2,"mobile", "laptop", "camera", "television",13202];
+    document.write("id : "+product[0]+ "<br/>"+ "Name : "+product[1]+"<br/>"+ "Price : "+product[5]+"<br/>");
+}
+javascriptArrayTry();
