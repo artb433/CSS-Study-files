b949f291abb4ae0831009009b585e43e10ea8f17
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-11 01:16:49 +0000

    Javascript operators

--- Section 2 - CSS Development/tryjavascript20.js
+++ Section 2 - CSS Development/tryjavascript20.js
@@ -4,9 +4,10 @@ function f8(){
     var isPermanentRole = true;
     var cities = ["Delhi","Hydrabad"];
 
+
     var getDetails = function(a,b){
         return a+b;
     }
-
-    document.write()
+    
+    document.write("Datatype of NAME: "+(typeof name)+"<br/>" + "Datatype of Age : "+(typeof age)+ "<br/>"+"Datatype of role: "+(typeof isPermanentRole)+"<br/>"+"Datatype of cities : "+(typeof cities));
 }
