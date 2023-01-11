82c4674c883c9aa04a0f7e265fe1ab39766b2477
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-11 23:06:15 +0000

    Javascript practice

---
+++ Section 2 - CSS Development/tryjavascript21.js
@@ -0,0 +1,18 @@
+function switchCase(){
+    var x=0;
+    switch(x)
+    {
+        case 0:
+            document.write("zero");
+        case 1:
+            document.write("one");
+        break;
+        case 2:
+            document.write("two");
+        break;
+        default:
+            document.write("Enter numbers 0 to 2");
+        break;
+    }
+}
+switchCase();
