245013fb066dd6a1fb4aefaf3a9bbbf753e04862
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-11 01:01:35 +0000

    Javascript operator practicee

--- Section 2 - CSS Development/tryjavascript20.js
+++ Section 2 - CSS Development/tryjavascript20.js
@@ -1,3 +1,12 @@
 function f8(){
-    
+    var name = "John";
+    var age = 23;
+    var isPermanentRole = true;
+    var cities = ["Delhi","Hydrabad"];
+
+    var getDetails = function(a,b){
+        return a+b;
+    }
+
+    document.write()
 }
