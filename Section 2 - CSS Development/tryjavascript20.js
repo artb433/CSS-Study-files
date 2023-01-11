f8e57bf871047cb78b4c1823c34833125cb9c9da
Tahiru Abdul Rahman <tahiruabdulrahmanbaaba3@gmail.com>
2023-01-11 23:06:36 +0000

    Javascript switch and break statement practice

--- Section 2 - CSS Development/tryjavascript20.js
+++ Section 2 - CSS Development/tryjavascript20.js
@@ -12,3 +12,32 @@ function f8(){
     document.write("Datatype of NAME: "+(typeof name)+"<br/>" + "Datatype of Age : "+(typeof age)+ "<br/>"+"Datatype of role: "+(typeof isPermanentRole)+"<br/>"+"Datatype of cities : "+(typeof cities));
 }
 f8();
+
+function selectHarddisk(){
+    var lstHdd=document.getElementById("lstHdd");
+    var txtRam=document.getElementById("txtRam");
+    if(parseInt(lstHdd.value)>=5){
+        txtRam.disabled=false;
+        lstHdd.disabled=true;
+    }
+}
+function selectRam(){
+    var txtRam=document.getElementById("txtRam");
+    var lstGraphic=document.getElementById("lstGraphic");
+    if(txtRam.value>=4){
+        lstGraphic.disabled=false;
+        txtRam.disable=true;
+    }
+}
+function selectGraphic(){
+    var lstGraphic=document.getElementById("lstGraphic");
+    var btnInstall=document.getElementById("btnInstall");
+    if(lstGraphic.value="yes"){
+        btnInstall.disabled=false;
+        lstGraphic.disabled=true;
+    }
+
+}
+function install(){
+    document.write("game installed successfully");
+}
